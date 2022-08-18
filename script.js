5311f6ce280b615c41c8ca3c4e7a4eeb792127e4
Maristane Figueiredo <maristanetecnologia@gmail.com>
2022-08-18 06:39:33 -0300

    cartas sortidas populando a dom

--- script.js
+++ script.js
@@ -1,67 +1,99 @@
+
+/*  
+Slice - excluir um determinado item do array;
+    nomeArray.slice(posicao,qtdeExclusao)
+Math.random ()  - valor aleatório gerado pela função entre 0 até 1. 
+
+
+*/
+
+
 let qtdeTotalCartas = prompt('Com quantas você deseja jogar?'); // 1
 const numeroMinCartas = 4;
 const numeroMaxCartas = 14;
 
 const cartasSelecionadas = [];
-const imagensFrentes = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif'
-    , 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif'];
+
+const imagensFrentes = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif',
+                        'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif'];
 
 
 
 
-// const imagensFrentes2 =    [ 'bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif'
-//     , 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif'];
+    while (qtdeTotalCartas % 2 !== 0) {
+        alert('Só é permitido números pares.'); //2
+        qtdeTotalCartas = prompt('Com quantas você deseja jogar? ');
+    }
 
-while (qtdeTotalCartas % 2 !== 0) {
-    alert('Só é permitido números pares.'); //2
-    qtdeTotalCartas = prompt('Com quantas você deseja jogar? ');
-}
-
-while ((qtdeTotalCartas < 4) || (qtdeTotalCartas > 14)) {
-    alert('A quantidade de cartas permitas para o jogo são de 4 até 14.');
-    qtdeTotalCartas = prompt('Com quantas você deseja jogar? ');
-}
-
-let contador = 0;
-while ((qtdeTotalCartas / 2) > contador) { //qtdeTotalCartas 4
-    const cartas = document.querySelector('.cartas');
-    console.log(cartas);
-    // const imagem = document.querySelector('.imagem');
-
-    const posicaoAleatoria = Math.floor(Math.random() * (imagensFrentes.length));
-    console.log(posicaoAleatoria)
-    cartasSelecionadas.push(imagensFrentes[posicaoAleatoria]);
-    cartasSelecionadas.push(imagensFrentes[posicaoAleatoria]);
-    console.log(cartasSelecionadas)
-    imagensFrentes.splice(posicaoAleatoria, 1);
-
-    console.log('imagensFrentes',imagensFrentes);
-
-    cartas.innerHTML = cartas.innerHTML + `<div class='carta' onclick='virarCarta(this)'>
-                                                <div class='cartaVerso'><img src="./imagens/front.png" ></div>                                                                                             
-                                                
-                                                <div class='cartaFrente'><img src="./imagens/${imagensFrentes[posicaoAleatoria]}" ></div>                                                 
-                                        </div>`;
-
-    cartas.innerHTML = cartas.innerHTML + `<div class='carta' onclick='virarCarta(this)'>
-                                        <div class='cartaVerso'><img src="./imagens/front.png" ></div>                                                                                             
-                                        
-                                        <div class='cartaFrente'><img src="./imagens/${imagensFrentes[posicaoAleatoria]}" ></div>                                                 
-                                </div>`;
+    while ((qtdeTotalCartas < 4) || (qtdeTotalCartas > 14)) {
+        alert('A quantidade de cartas permitas para o jogo são de 4 até 14.');
+        qtdeTotalCartas = prompt('Com quantas você deseja jogar? ');
+    }
 
 
-    /* <div class='cartaFrente'><img src="./imagens/fiestaparrot.gif" ></div>  */
-    contador++;
-    // cartasSolicitadas.push();
-    // cartas.innerHTML = cartas.innerHTML + `<li>${cartasSolicitadas[contador]}</li>`; 
-}
+
+    let contador = 0;
+    while ((qtdeTotalCartas / 2) > contador) { //qtdeTotalCartas 4 entrará 2x
+        cartas = document.querySelector('.cartas');   
+
+
+        //Pega uma posição aleatória no array 
+        const posicaoAleatoria = Math.floor(Math.random() * (imagensFrentes.length));
+    
+
+        //Adicona no array "carta selecionas" -a mesma quarta duas vezes
+        cartasSelecionadas.push(imagensFrentes[posicaoAleatoria]);
+        cartasSelecionadas.push(imagensFrentes[posicaoAleatoria]);
+    
+
+        //Retira a imagem do array "imagensFrentes" para a mesma não ser escolhida outra vez no modo randômico. 
+        imagensFrentes.splice(posicaoAleatoria, 1);
+        
+     
+        contador++;
+    }
+
+
+
+     // Esta função pode ficar separada do código acima, onde você preferir
+    function comparador() { 
+        return Math.random() - 0.5; 
+    }                        
+
+    cartasSelecionadas.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
+   
+    
+    
+
+    // Populando a DOM - divs possuirão as cartas   
+    for(let i = 0; i < cartasSelecionadas.length; i++) {    
+
+        cartas.innerHTML = cartas.innerHTML + `
+                                          <div class='carta' onclick='virarCarta(this)'>
+                                              <div class='cartaVerso'><img src="./imagens/front.png" ></div>                                                                                                                                             
+                                              
+                                              <div class='cartaFrente'><img src="./imagens/${cartasSelecionadas[i]}" ></div> 
+                                             
+                                          </div>
+                                        `;                                      
+    }
+
+
+
+// function virarCarta(carta) {
+//     const cartaVerso = carta.querySelector('.cartaVerso');
+//     cartaVerso.style.display = "none";
+
+//     const cartaFrente = carta.querySelector('.cartaFrente');
+//     cartaFrente.style.display = "block";
+// }
+
 
 function virarCarta(carta) {
-    const cartaVerso = carta.querySelector('.cartaVerso');
-    cartaVerso.style.display = "none";
-
-    const cartaFrente = carta.querySelector('.cartaFrente');
-    cartaFrente.style.display = "block";
-
-
+         const cartaVerso = carta.querySelector('.cartaVerso');
+         cartaVerso.style.display = "none";
+    
+         const cartaFrente = carta.querySelector('.cartaFrente');
+         cartaFrente.style.display = "block";
 }
+    
